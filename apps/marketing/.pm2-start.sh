da582f27df302cdade24a335e0cd0a9e4b63fc5f
Bhaskar Anand <bhaskar@talproindia.com>
2026-06-02 09:54:50 +0530

    fix(marketing): bind pm2 start to loopback

--- apps/marketing/.pm2-start.sh
+++ apps/marketing/.pm2-start.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")"
 
 export NODE_ENV="${NODE_ENV:-production}"
 export PORT="${PORT:-5110}"
-export HOSTNAME="${HOSTNAME:-127.0.0.1}"
+HOST="${QORIUM_MARKETING_HOST:-127.0.0.1}"
 
-exec ./node_modules/next/dist/bin/next start -H "$HOSTNAME" -p "$PORT"
+exec ./node_modules/next/dist/bin/next start -H "$HOST" -p "$PORT"
